59ec183bf8066c68e571bb0b297eebf21bf26d99
alex <alexisoro99@gmail.com>
2023-08-15 20:40:38 -0700

    small change

--- part1/app.js
+++ part1/app.js
@@ -2,15 +2,17 @@ let favNumber = 0;
 let baseURL = "http://numbersapi.com";
 
 // 1st
-$.getJSON(`${baseURL}/${favNumber}?json`).then(singleData => {
-  console.log(singleData);
-});
+$.getJSON(`${baseURL}/${favNumber}?json`)
+  .then(singleData => {
+    console.log(singleData);
+  });
 
 // 2nd
 let favNumbers = [7, 11, 22];
-$.getJSON(`${baseURL}/${favNumbers}?json`).then(arrayData => {
-  console.log(arrayData);
-});
+$.getJSON(`${baseURL}/${favNumbers}?json`)
+  .then(arrayData => {
+    console.log(arrayData);
+  });
 
 // 3rd
 const numberOfRequests = 4;
@@ -18,6 +20,7 @@ const factPromises = Array.from({ length: numberOfRequests }, () => {
   return $.getJSON(`${baseURL}/${favNumber}?json`);
 });
 
-Promise.all(factPromises).then(facts => {
-  facts.forEach(data => $("body").append(`<p>${data.text}</p>`));
-});
+Promise.all(factPromises)
+  .then(facts => {
+    facts.forEach(data => $("body").append(`<p>${data.text}</p>`));
+  });
